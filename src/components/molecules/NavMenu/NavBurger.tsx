eab51bf931c2734b814b265bbf8d9a5f1a3a9c2c
KrNel <159314411+krisbelangernelson@users.noreply.github.com>
2024-03-22 23:30:41 -0400

    refactor: nav default server, state into client

---
+++ src/components/molecules/NavMenu/NavBurger.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { Dispatch, SetStateAction } from 'react'
+
+interface NavMobileProps {
+  toggleMenu: boolean
+  setToggleMenu: Dispatch<SetStateAction<boolean>>
+}
+
+export default function NavBurger(props: NavMobileProps) {
+  const { setToggleMenu, toggleMenu } = props
+
+  const burgerLine = 'h-1 w-6 my-1 rounded-full bg-black transition ease transform duration-300 primary-bg'
+
+  return (
+    <div className="flex items-center justify-center">
+      <button
+        className="flex flex-col h-12 w-12 border-2 border-black rounded justify-center items-center group primary-border"
+        onClick={() => setToggleMenu(!toggleMenu)}
+      >
+        <div
+          className={`${burgerLine} ${toggleMenu
+            ? 'rotate-45 translate-y-3 opacity-50 group-hover:opacity-100'
+            : 'opacity-50 group-hover:opacity-100'}`}
+        />
+        <div
+          className={`${burgerLine} ${toggleMenu ? 'opacity-0' : 'opacity-50 group-hover:opacity-100'}`}
+        />
+        <div
+          className={`${burgerLine} ${toggleMenu
+            ? '-rotate-45 -translate-y-3 opacity-50 group-hover:opacity-100'
+            : 'opacity-50 group-hover:opacity-100'}`}
+        />
+      </button>
+    </div>
+  )
+}
--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,11 +1,4 @@
-import { type ReactElement } from 'react'
-// import Stack from 'react-bootstrap/Stack'
 import NavMenu from '@/components/molecules/NavMenu/NavMenu'
-// import MyAccount from '@/components/molecules/MyAccount/MyAccount'
-// import Language from '@/components/molecules/Language/Language'
-// import Container from 'react-bootstrap/Container'
-// import Row from 'react-bootstrap/Row'
-// import Col from 'react-bootstrap/Col'
 
 export default function Header() {
   return (
