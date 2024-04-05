e691a992cd0b167d350092a2cd173035eda33cdf
KrNel <159314411+krisbelangernelson@users.noreply.github.com>
2024-04-05 08:45:30 -0400

    feat: transition on visibility

--- src/components/pages/Home/FeaturesItem.tsx
+++ src/components/pages/Home/FeaturesItem.tsx
@@ -1,8 +1,10 @@
 'use client'
 
 import { ROUTES } from '@/constants'
+import { useIsVisible } from '@/hooks/useIsVisible'
 import Image, { StaticImageData } from 'next/image'
 import { useRouter } from 'next/navigation'
+import { useRef } from 'react'
 
 interface FeaturesItemProps {
   imgSrc: StaticImageData
@@ -15,10 +17,12 @@ interface FeaturesItemProps {
 export default function FeaturesItem(props: FeaturesItemProps) {
   const { imgSrc, heading, subHeading, textOrder, imageOrder } = props
   const router = useRouter()
+  const ref1 = useRef() as React.MutableRefObject<HTMLDivElement>
+  const isVisible1 = useIsVisible(ref1)
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 items-center md:gap-4">
-      <div className={`sm:w-[32rem] md:order-${imageOrder}`}>
+      <div className={`sm:w-[32rem] md:order-${imageOrder} transition-opacity ease-in duration-700 ${isVisible1 ? 'opacity-100' : 'opacity-0'}`} ref={ref1}>
         <Image
           loading="lazy"
           src={imgSrc}
