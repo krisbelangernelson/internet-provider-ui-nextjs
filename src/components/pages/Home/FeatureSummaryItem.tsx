69f37e9732c2b74618003c3d360e88263d120388
krisbelangernelson <159314411+krisbelangernelson@users.noreply.github.com>
2024-04-07 14:14:54 -0400

    fix: overflow no scrollbar for feature images (#16)

--- src/components/pages/Home/FeatureSummaryItem.tsx
+++ src/components/pages/Home/FeatureSummaryItem.tsx
@@ -12,15 +12,12 @@ export default function FeaturesSummaryItem(props: FeaturesSummaryItemProps) {
   return (
     <div className="">
       <div className="grid grid-cols-12">
-
         <Image src={imgSrc} loading="lazy" alt="Feature image" className="col-span-3 text-end mt-2 justify-self-end me-4" />
-
         <div className="col-span-9 text-start">
           <AnimateIn>
             <h4>{heading}</h4>
             <p>{subHeading}</p>
           </AnimateIn>
-
         </div>
       </div>
     </div>
